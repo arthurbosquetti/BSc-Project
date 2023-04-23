45b1eb65c478a4ecfa3ca52e7d4388d18030998c
arthurbosquetti <arthur.bosquetti@gmail.com>
2023-04-23 11:51:31 +0200

    Added tabs to navigate through project page

--- client-app/src/components/ProjectPage.vue
+++ client-app/src/components/ProjectPage.vue
@@ -1,21 +1,49 @@
 <template>
     <div class="marginated">
-        Here you can find everything about {{ projectName }}!
+        <b-tabs
+        active-nav-item-class="font-weight-bold text-primary" content-class="mt-3">
+            <b-tab title="Project Overview"><p>Nothing to show here yet.</p></b-tab>
+
+            <b-tab title="SV FFT Trends">
+                <GraphsTestDataEntries />
+            </b-tab>
+            
+            <b-tab title="Bug Trends">
+                <GraphsBugDataEntries />
+            </b-tab>
+
+            <b-tab title="SV FFT Data Table">
+                Add something to export the data?
+                <ListTestDataEntries />
+            </b-tab>
+            <b-tab title="Bug Data Table">
+                Add something to export the data?
+                <ListBugDataEntries />
+            </b-tab>
+            <b-tab title="Settings">
+                <p>Nothing to show here yet.</p>
+            </b-tab>
+        </b-tabs>
+
     </div>
 </template>
 
 <script>
+import GraphsBugDataEntries from './GraphsBugDataEntries.vue';
+import GraphsTestDataEntries from './GraphsTestDataEntries.vue';
+import ListTestDataEntries from './ListTestDataEntries.vue';
+import ListBugDataEntries from './ListBugDataEntries.vue';
+
 export default {
-    name: 'ProjectPage',
+    name: "ProjectPage",
+    components: { GraphsTestDataEntries, GraphsBugDataEntries, ListTestDataEntries, ListBugDataEntries },
     data() {
         return {
-            projectName: '',
+            projectName: "",
         }
     },
     async mounted() {
-        this.projectName = this.$route.params.projectName
+        this.projectName = this.$route.params.projectName;
     }
 }
-
-
 </script>
