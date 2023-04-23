20828c569769777375d1addfd4c97cae39740b53
arthurbosquetti <arthur.bosquetti@gmail.com>
2023-04-23 13:48:21 +0200

    Added icons to tabs in project page

--- client-app/src/components/ProjectPage.vue
+++ client-app/src/components/ProjectPage.vue
@@ -2,25 +2,48 @@
     <div class="marginated">
         <b-tabs
         active-nav-item-class="font-weight-bold text-primary" content-class="mt-3">
-            <b-tab title="Project Overview"><p>Nothing to show here yet.</p></b-tab>
+            <b-tab>
+                <template #title>
+                    Overview<b-icon icon="info-circle" variant="primary" class="ml-2"></b-icon>
+                </template>
+                <p>Nothing to show here yet.</p>
+            </b-tab>
 
-            <b-tab title="SV FFT Trends">
+            <b-tab>
+                <template #title>
+                    SV FFT Trends <b-icon icon="graph-up" variant="primary" class="ml-2"></b-icon>
+                </template>
                 <GraphsTestDataEntries />
             </b-tab>
             
-            <b-tab title="Bug Trends">
+            <b-tab>
+                <template #title>
+                    Bug Trends <b-icon icon="graph-down" variant="primary" class="ml-2"></b-icon>
+                </template>
                 <GraphsBugDataEntries />
             </b-tab>
 
-            <b-tab title="SV FFT Data Table">
+            <b-tab>
+                <template #title>
+                    SV FFT Data <b-icon icon="clipboard-data" variant="primary" class="ml-2"></b-icon>
+                </template>
                 <ListTestDataEntries />
             </b-tab>
-            <b-tab title="Bug Data Table">
+
+            <b-tab>
+                <template #title>
+                    Bug Data <b-icon icon="clipboard-data" variant="primary" class="ml-2"></b-icon>
+                </template>
                 <ListBugDataEntries />
             </b-tab>
-            <b-tab title="Settings">
+
+            <b-tab>
+                <template #title>
+                    Settings <b-icon icon="gear" variant="primary" class="ml-2"></b-icon>
+                </template>
                 <p>Nothing to show here yet.</p>
             </b-tab>
+
         </b-tabs>
 
     </div>
